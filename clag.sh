6b2f2bd1be47e28b6e5de788f133bca1e520c571
Paweł Rubach <pawel.rubach@gmail.com>
2024-02-16 16:52:03 +0100

    Cluster agent start script

--- clag.sh
+++ clag.sh
@@ -4,6 +4,7 @@
 #SBATCH --mem=1gb
 #SBATCH --partition=long
 #SBATCH --output=ClstAgnt_slurm.out
-source $HOME/dev/ksa_demo/venv/bin/activate
-cd $HOME/dev/ksa_demo
+KSA_DIR=$HOME/dev/ksa_demo
+source $KSA_DIR/venv/bin/activate
+cd $KSA_DIR
 faust -A kafka_slurm_agent.cluster_agent -l info worker > logs/cluster_agent_out.txt
