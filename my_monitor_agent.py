4013743a3d351df24c2cc842508596eb1032136d
Pawel Rubach <gnu9yh@virginia.edu>
2024-11-21 13:00:41 -0500

    Better output monitor

--- my_monitor_agent.py
+++ my_monitor_agent.py
@@ -6,4 +6,4 @@ from kafka_slurm_agent.monitor_agent import app, job_status, done_topic
 @app.agent(done_topic)
 async def process_done(stream):
 	async for msg in stream.events():
-		print('Got {}: {}'.format(msg.key, msg.value))
+		print('Got {}: {}'.format(msg.key.decode('utf-8'), msg.value))
